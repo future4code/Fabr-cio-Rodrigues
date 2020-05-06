1adef597d16d4f27afa9578ecd3addd0c091f2d2
ProgFabs <earthbornshepard@hotmail.com>
2020-05-06 20:25:22 -0300

    exercícios concluídos, desafios parcialmente concluídos

--- semana4/objetos/blog/scripts.js
+++ semana4/objetos/blog/scripts.js
@@ -1,35 +1,55 @@
-function buttonClick() {
-        let postConfirmation = confirm("Tem certeza que deseja postar essa publicação na sua linha do tempo?");
-        if (postConfirmation == true) {
-            post();
-        } else {
-            console.log("Confirmação negada.")
-        }
-    }
+let posts = [];
+let postID = 0;
+
+// OBJETO
+
+const post = {
+    title: document.getElementById("post-title"),
+    author: document.getElementById("post-author"),
+    text: document.getElementById("post-input"),
+    image: document.getElementById('post-image'),
+}
+
+function publish() {
+    const postTitle = post.title;
+    const postAuthor = post.author;
+    const postText = post.text;
+    const postImage = post.image;
+
+    let postPublishing = document.getElementById("post");
+    postPublishing.innerHTML += "<div class='new-post " + 'post' + postID + "'>"
+    postPublishing.innerHTML += "</div>"
+
+    let postContent = document.querySelector(`.post, .post${postID}`);
+ 
+    postContent.innerHTML += "<div class='post-title'>" + postTitle.value + "</div>";
+    postContent.innerHTML += "<div class='post-author'>" + postAuthor.value + "</div>";
+    postContent.innerHTML += "<div class='post-image'>" + "<img src='" + postImage.value +"'/>" + "</div>";
+    postContent.innerHTML += "<div class='post-text'>" + postText.value + "</div>";
+
+    postText.value = '';
+    postAuthor.value = '';
+    postImage.value = '';
+    postTitle.value = '';
+    postID++;
+}
 
 function onEnterPress(event) {
-    if(event.key === 'Enter') {
-        postConfirmation = confirm("Tem certeza que deseja postar essa publicação na sua linha do tempo?"); 
+    if (event.key === 'Enter') {
+        postConfirmation = confirm("Tem certeza que deseja postar essa publicação na sua linha do tempo?");
         if (postConfirmation == true) {
-            post();
-        } else {
+            publish();
+        } else if (postConfirmation == false) {
             console.log("Confirmação negada.")
         }
     }
 }
 
-function post() {
-    const postTitle = document.getElementById("post-title")
-    const postAuthor = document.getElementById("post-author");
-    const myPost = document.getElementById("post-input");
-
-    const postPublishing = document.getElementById("post");
-
-    postPublishing.innerHTML += "<div class='post-title'>" + postTitle.value + "</div>";
-    postPublishing.innerHTML += "<div class='post-author'>" + "Escrito por: " + postAuthor.value + "</div>";
-    postPublishing.innerHTML += "<div class='post'>" + myPost.value + "</div>";
-
-    myPost.value = '';
-    postAuthor.value = '';
-    postTitle.value = '';
+function buttonClick() {
+    let postConfirmation = confirm("Tem certeza que deseja postar essa publicação na sua linha do tempo?");
+    if (postConfirmation == true) {
+        publish();
+    } else {
+        console.log("Confirmação negada.")
+    }
 }
