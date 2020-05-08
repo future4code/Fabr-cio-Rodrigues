67da353eb915fc8a2c277cdf6af9a917dc2d4f48
ProgFabs <earthbornshepard@hotmail.com>
2020-05-07 23:43:10 -0300

    exercício incompleto (faltando um item)

--- semana4/funcoes-de-array/scripts.js
+++ semana4/funcoes-de-array/scripts.js
@@ -1,5 +1,6 @@
 /* CADASTRO */
-//FUNCIONALIDADES FALTANDO: submit com enter, limpar inputs a cada submit.
+let contadorArray = 1;
+let i = 0;
 
 const controlador = {  
     valor: document.getElementById("controlador-valor"),
@@ -7,7 +8,9 @@ const controlador = {
     descricao: document.getElementById("controlador-descricao"),
 }
 
-let cadastros = []
+let cadastros = [];
+let valorTotal = 0;
+
 
 // sintaxe de push: cadastros.push({valor: x, tipo: y, descricao: z})
 
@@ -17,7 +20,7 @@ const cadastrar = ((cadastro => {
     let descricaoCadastrada = controlador.descricao.value;
 
     cadastro = {
-      valor: "R$" + valorCadastrado,
+      valor: valorCadastrado,
       tipo: tipoCadastrado,
       descricao: descricaoCadastrada,
     };
@@ -30,23 +33,68 @@ const cadastrar = ((cadastro => {
 
 }))
 
-const despesasDetalhadas = (() =>{
-    let display = cadastros.text;
-    let despesaDetalhada = document.getElementById("despesas-txtbox");
+/* Despesas */
+// FUNCIONALIDADES FALTANDO: Filter.
 
-    despesaDetalhada.innerHTML += "despesas" + cadastros.valor;
-    
+const despesaPorTipo = (() => {
+    let despesaDetalhada = document.getElementById("despesas-lista");
+    let d = document.getElementById("despesas-tipo");
+    let tipoSelecionado = d.options[d.selectedIndex].value;
+
+    const despesaPorTipo = cadastros.filter((despesa) => {
+        tipoSelecionado2 = despesa.tipo === `${tipoSelecionado}`;
+    }); 
+
+    for (i; i <= contadorArray;) {
+        despesaDetalhada.innerHTML += `<div>${cadastro[i].valor}</div>`;
+    }
 })
 
+const despesasDetalhadas = (() =>{
+    let despesaDetalhada = document.getElementById("despesas-lista");
+    let valorExtrato = document.getElementById("valor-total");
+
+
+
+    for (i; i <= contadorArray;) {
+        despesaDetalhada.innerHTML += "<li id='despesas-lista-header'>" + "<p>" + `R$${cadastros[i].valor} ` + "</p>" + "<p>" + `${cadastros[i].tipo}` + "</p>" + "<p>" + `${cadastros[i].descricao}` + "</p>" + "</li>";
+        valorTotal += Number(cadastros[i].valor); 
+        contadorArray++;
+        i++;
+        valorExtrato.innerHTML = valorTotal;
+    }
+
+})
+
+function enterBotaoCadastro(event) {
+    if (event.key === 'Enter') {
+    let postConfirmation = confirm("Tem certeza que deseja cadastrar esta despesa?");
+    if (postConfirmation == true) {
+      cadastrar();
+      despesasDetalhadas();
+    } else if (postConfirmation == false) {
+      console.log("Confirmação negada.");
+    }
+  }
+}
+
 function cliqueBotaoCadastro() {
-  let subscriptionConfirmation = confirm(
-    "Tem certeza que deseja cadastrar esta despesa?"
-  );
-  if (subscriptionConfirmation == true) {
+    postConfirmation = confirm("Tem certeza que deseja cadastrar esta despesa?");
+  if (postConfirmation == true) {
     cadastrar();
     despesasDetalhadas();
-
   } else {
     console.log("Confirmação negada.");
   }
 }
+
+function cliqueDespesasDetalhadas() {
+  postConfirmation = confirm("Tem certeza que deseja cadastrar esta despesa?");
+  if (postConfirmation == true) {
+    despesaPorTipo();
+  } else {
+    console.log("Confirmação negada.");
+  }
+}
+
+
