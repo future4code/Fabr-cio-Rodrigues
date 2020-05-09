327c97b3501c9db322bf62dd3998317b531c32d0
ProgFabs <earthbornshepard@hotmail.com>
2020-05-09 18:02:32 -0300

    exercício 20 concluído

--- semana4/lista-de-exercicios/scripts.js
+++ semana4/lista-de-exercicios/scripts.js
@@ -341,3 +341,23 @@ function enviaEmail() {
 */
 
 /* EXERCÍCIO 5 */
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+];
+
+contas.forEach((conta, index) => {
+    for (compra in conta.compras) {
+        let sum = 0;
+        let i = conta.compras.length;
+        while (i--) sum += conta.compras[i];
+        console.log(sum)
+        conta.saldoTotal -= sum;
+        break;
+        }
+    })
+
