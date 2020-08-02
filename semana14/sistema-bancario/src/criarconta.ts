3cc8f104df5170176eadea9a90345eb5a6098d08
Fabricio <earthbornshepard@hotmail.com>
2020-08-02 17:25:57 -0300

    exercícios concluídos

--- semana14/sistema-bancario/src/criarconta.ts
+++ semana14/sistema-bancario/src/criarconta.ts
@@ -1,49 +1,57 @@
 import * as fs from "fs";
 import moment from "moment";
-type account = {
-  nome: string;
+
+type Account = {
+  name: string;
   cpf: string;
-  dataNascimento: string;
-  saldo: number;
+  userBirthday: moment.Moment;
+  balance: number;
+  extract: [];
 };
+
+const receivedAccounts: string = fs.readFileSync("./data.json").toString();
+const updatedAccounts: Account[] = receivedAccounts
+  ? JSON.parse(receivedAccounts)
+  : [];
 
 const newArgs = process.argv.slice(1);
 const insertedName: string = newArgs[1];
 const insertedCpf: string = newArgs[2];
 const insertedBirthday: string = newArgs[3];
 
-let accounts;
-
-export function writeToDatabase(data: any): void {
-  try {
-    const dataAsString: string = JSON.stringify(data, null, 2);
-    fs.appendFileSync("./data.json", `\n${dataAsString}`);
-  } catch (error) {
-    console.log("Erro ao salvar os dados: " + error.message);
+const checkCpf = (cpf: string): boolean => {
+  let data: Account[] = JSON.parse(fs.readFileSync("./data.json").toString());
+  for (let user of data) {
+    if (user.cpf === cpf) {
+      return true;
+    }
   }
-}
+  return false;
+};
 
-export function readDatabase(): any {
-  try {
-    const fileData: string = fs.readFileSync("./data.json").toString();
-    JSON.parse('{"rightQuotes": 5}');
-    return JSON.parse(fileData);
-  } catch (error) {
-    console.log("Erro ao ler a base de dados: " + error.message);
-    return [];
+const createNewAccount = (): void => {
+  if (!insertedName || !insertedBirthday || !insertedCpf) {
+    console.log("Oops, informações inválidas! Tente novamente.");
+  } else if (checkCpf(insertedCpf) === true) {
+    console.log(
+      `O CPF ${insertedCpf} já está cadastrado! Tente novamente com outro CPF ou faça login usando este CPF.`
+    );
+  } else {
+    let newAccount: Account = {
+      name: insertedName,
+      cpf: insertedCpf,
+      userBirthday: moment(insertedBirthday, "DD/MM/YYYY"),
+      balance: 0,
+      extract: [],
+    };
+
+    let newAccounts = [...updatedAccounts, newAccount];
+
+    const dataAsString: string = JSON.stringify(newAccounts, null, 2);
+    fs.writeFileSync("./data.json", `\n${dataAsString}`);
+
+    console.log("Nova conta criada com sucesso!");
   }
-}
-
-const createNewAccount = () => {
-  let newAccount: account = {
-    nome: insertedName,
-    cpf: insertedCpf,
-    dataNascimento: insertedBirthday,
-    saldo: 0,
-  };
-
-  console.log("Nova conta criada com sucesso!");
-  writeToDatabase(newAccount);
 };
 
 createNewAccount();
